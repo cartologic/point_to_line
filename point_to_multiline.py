342ef64ef2a156104c979adff4e96e6624267842
khaledboka <bokae97@gmail.com>
2019-08-27 12:43:05 +0200

    Implemented: Select Features Collection from input point layer
    Ex:
    1. User selects a point layer and selects group by attribute
    2. The Class PointToMultiline will calculate the possible output line features based on the group by attribute
    3. User selects some or all and publish to database

--- point_to_multiline.py
+++ point_to_multiline.py
@@ -2,11 +2,21 @@ from osgeo import ogr, osr
 
 
 class PointsToMultiPath(object):
-    def __init__(self, connection_string, in_layer_name, out_layer_name, sort_by_attr, group_by_attr):
+    def __init__(self,
+                 connection_string,
+                 in_layer_name,
+                 out_layer_name,
+                 sort_by_attr,
+                 group_by_attr,
+                 line_features_list=None
+                 ):
         self.in_layer_name = str(in_layer_name)
         self.out_layer_name = str(out_layer_name)
-        self.sort_by_attr = str(sort_by_attr) if sort_by_attr is not None else None
-        self.group_by_attr = str(group_by_attr) if group_by_attr is not None else None
+        self.sort_by_attr = str(
+            sort_by_attr) if sort_by_attr is not None else None
+        self.group_by_attr = str(
+            group_by_attr) if group_by_attr is not None else None
+        self.line_features_list = line_features_list if line_features_list is not None else None
         self.connection_string = connection_string
 
     def start_connection(self):
@@ -14,9 +24,7 @@ class PointsToMultiPath(object):
 
     def execute(self):
         self.get_in_layer()
-        self.group_by_index = self.get_field_index(self.group_by_attr) if self.group_by_attr is not None else None
-        self.sort_by_index = self.get_field_index(self.sort_by_attr) if self.sort_by_attr is not None else None
-        
+
         self.create_out_layer()
         self.features_dict = self.create_features_dict()
         self.out_features = self.create_out_features()
@@ -24,27 +32,30 @@ class PointsToMultiPath(object):
         self.commit_transactions()
 
     def get_in_layer(self):
-        self.in_layer = self.conn.GetLayer( self.in_layer_name )
+        self.in_layer = self.conn.GetLayer(self.in_layer_name)
         self.in_layerDefn = self.in_layer.GetLayerDefn()
         self.srs = self.in_layer.GetSpatialRef()
-            
+        self.group_by_index = self.get_field_index(
+            self.group_by_attr) if self.group_by_attr is not None else None
+        self.sort_by_index = self.get_field_index(
+            self.sort_by_attr) if self.sort_by_attr is not None else None
+
     def create_out_layer(self):
         # create an out layer in postgres
         self.conn.CreateLayer(
-            self.out_layer_name, 
-            geom_type=ogr.wkbLineString, 
-            srs=self.srs, 
+            self.out_layer_name,
+            geom_type=ogr.wkbLineString,
+            srs=self.srs,
             options=['OVERWRITE=YES']
         )
-        self.out_layer = self.conn.GetLayer( self.out_layer_name )
+        self.out_layer = self.conn.GetLayer(self.out_layer_name)
         self.out_featureDefn = self.out_layer.GetLayerDefn()
-    
 
     def get_field_index(self, attr):
-        for i in range( self.in_layerDefn.GetFieldCount() ):
-            if (self.in_layerDefn.GetFieldDefn( i ).GetName() == attr):
+        for i in range(self.in_layerDefn.GetFieldCount()):
+            if (self.in_layerDefn.GetFieldDefn(i).GetName() == attr):
                 return i
-        
+
     def create_features_dict(self):
         features_dict = {}
         # consider all point features in one line feature if not (group by)
@@ -53,15 +64,30 @@ class PointsToMultiPath(object):
             return features_dict
         for f in self.in_layer:
             line_name = f[self.group_by_index]
-            try:
-                # if the list of features
-                features_dict[line_name].append(f)
-            except:
-                # create list of features if not existing
-                features_dict[line_name] = []
-                features_dict[line_name].append(f)
+            # If user selected some line features   
+            if self.line_features_list:
+                # if line feature in the selected line features
+                if line_name in self.line_features_list:
+                    try:
+                        # if the list of features
+                        features_dict[line_name].append(f)
+                    except:
+                        # create list of features if not existing
+                        features_dict[line_name] = []
+                        features_dict[line_name].append(f)
+                else:
+                    # TODO: raise error or handle skipping
+                    pass
+            # Consider all line features are selected
+            else:
+                try:
+                    # if the list of features
+                    features_dict[line_name].append(f)
+                except:
+                    # create list of features if not existing
+                    features_dict[line_name] = []
+                    features_dict[line_name].append(f)
         return features_dict
-    
 
     def create_out_features(self):
         # Lines Creation Process:
@@ -77,16 +103,15 @@ class PointsToMultiPath(object):
             for feat in sorted_features:
                 geom = feat.GetGeometryRef()
                 line.AddPoint(geom.GetX(), geom.GetY())
-            
-            # Create a new feature 
+
+            # Create a new feature
             out_feature = ogr.Feature(self.out_featureDefn)
-            
+
             # Set Feature Geometry
             out_feature.SetGeometry(line)
-            
+
             out_features.append(out_feature)
         return out_features
-
 
     def commit_transactions(self):
         # Start transaction with postgres and create feature(table row)
