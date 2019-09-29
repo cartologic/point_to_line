39c9b96c8fbf384c4b01271463330a44ad0e71ff
khaledboka <bokae97@gmail.com>
2019-09-29 14:33:07 +0200

    append duplicate features to the get line features result

--- views.py
+++ views.py
@@ -25,46 +25,39 @@ def index(request):
 @login_required
 def get_line_features(request):
     if request.method == 'POST':
-        form = PointToLineForm(request.POST)
-        if form.is_valid():
-            # 1. Check clean form
-            # 2. Check table exist
-            in_layer_name = form.cleaned_data['in_layer_name']
-            out_layer_name = form.cleaned_data['out_layer_name']
-            sort_by_attr = form.cleaned_data['sort_by_attr']
-            group_by_attr = form.cleaned_data['group_by_attr']
-            if table_exist(out_layer_name):
-                json_response = {"status": False,
-                                 "message": "Layer Already Exists!, Try again with different layer name, If you don't see the existing layer in the layer list, Please contact the administrator", }
-                return JsonResponse(json_response, status=500)
-            # 3. Get current line features
-            connection_string = create_connection_string()
-            p = PointsToMultiPath(
-                connection_string,
-                in_layer_name,
-                out_layer_name,
-                sort_by_attr=sort_by_attr,
-                group_by_attr=group_by_attr,
-            )
-            p.start_connection()
-            p.get_in_layer()
-            features_dict = p.create_features_dict()
-            features_dict_res = [
-                {
-                    "name": key,
-                    "numberOfFeatures": len(value)
-                }
-                for key, value in features_dict.iteritems()
-            ]
-            p.close_connection()
-            json_response = {
-                'status': True,
-                'objects': features_dict_res
+        in_layer_name = request.POST.get('in_layer_name')
+        sort_by_attr = request.POST.get('sort_by_attr')
+        group_by_attr = request.POST.get('group_by_attr')
+        # 3. Get current line features
+        connection_string = create_connection_string()
+        p = PointsToMultiPath(
+            connection_string,
+            in_layer_name,
+            None,
+            sort_by_attr=sort_by_attr,
+            group_by_attr=group_by_attr,
+        )
+        p.start_connection()
+        p.get_in_layer()
+        features_dict = p.create_features_dict()
+        duplicates_dict = p.get_duplicated_features()
+        features_dict_res = [
+            {
+                "name": key,
+                "numberOfFeatures": len(value),
+                "duplicated_features": duplicates_dict[key]['duplicate']
             }
-            return JsonResponse(json_response, status=200)
-        json_response = {"status": False,
-                         "message": "Error while getting line features, Form is not valid!", }
-        return JsonResponse(json_response, status=500)
+            for key, value in features_dict.iteritems()
+        ]
+        p.close_connection()
+        json_response = {
+            'status': True,
+            'objects': features_dict_res
+        }
+        return JsonResponse(json_response, status=200)
+    json_response = {"status": False,
+                        "message": "Error while getting line features, Form is not valid!", }
+    return JsonResponse(json_response, status=500)
 
 
 @login_required
